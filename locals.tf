ab7409f25dd3369d245332050e815df971bd28a7
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-04-26 14:34:40 +0530

    Change: condition "" to null

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-  environment = var.environment != "" ? "-${var.environment}" : ""
-  aws_estate  = var.aws_estate != "" ? "-${var.aws_estate}" : ""
+  environment = var.environment != null ? "-${var.environment}" : ""
+  aws_estate  = var.aws_estate != null ? "-${var.aws_estate}" : ""
 
   bucket_name         = "${var.project_name}${local.aws_estate}${local.environment}-tfstate"
   dynamodb_table_name = "${var.project_name}${local.environment}-tfstate-lock"
